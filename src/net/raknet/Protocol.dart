d319d4392ff8bc46507a10fb8b415d8e90b150ff
Dylan Ogle <delxhq@funo.network>
2019-02-05 18:16:33 +0000

    Change BedrockVersion to 1.9

--- src/net/raknet/Protocol.dart
+++ src/net/raknet/Protocol.dart
@@ -2,7 +2,7 @@ class Protocol {
   static const int ServerId = 925686942;
   static const int ProtocolVersion = 9;
   static const int SystemAddresses = 10;
-  static const String BedrockVersion = '1.8.1';
+  static const String BedrockVersion = '1.9';
 
   static const List<int> Magic = [0x00, 0xff, 0xff, 0x00, 0xfe, 0xfe, 0xfe, 0xfe, 0xfd, 0xfd, 0xfd, 0xfd, 0x12, 0x34, 0x56, 0x78];
 
